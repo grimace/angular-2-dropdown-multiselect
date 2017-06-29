b5c37bb9fd5fe4920ab3ef998ff2dc2474650ab2
Phoenix Consulting <phoenix@Gregorys-MacBook-Pro-2.local>
2017-06-29 11:52:58 -0700

    More Dist

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+export * from './dropdown/types';
+export * from './dropdown/search-filter.pipe';
+export * from './dropdown/dropdown.module';
+export * from './dropdown/dropdown.component';
+export * from './dropdown/accordion/accordion.component';
+//# sourceMappingURL=index.js.map
